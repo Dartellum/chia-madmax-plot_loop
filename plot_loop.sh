609c3ab5c7e5754302f5411941178fe5030c24db
Dr. Walter Wesley Snyder V <wessnyder@comcast.net>
2021-06-24 15:07:22 -0400

    Added pass in variable for count so one does not need to edit the file after initial setup.

--- plot_loop.sh
+++ plot_loop.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 # Variables
-dest=""
+dest="" # Need to reset to empty
+
+# Only arg passed in is how many times to run, if blank, uses default in this script
+if [ ! -z $1 ];
+then
+  count=$1 # $1 was given
+else
+  count=2 # $1 was not given
+fi
+echo "count is $count."
+threads=4
+buckets=256
+buckets3=${buckets}
 tempdir=/media/plots-02/temp/
 tempdir2=/media/plots/temp/
 pool=
 farm=
+tmptoggle=false
 log=/home/chia/chialogs
 dest1=/media/plots-02
 dest2=/media/plots-03
@@ -26,14 +39,14 @@ disk_space () {
        echo "The Mount Point ${j} on $(hostname) has used ${used} at $1." |tee -a ${log}/${1}_plot-${2}.log;
      else
        dest=${j}/${farm_folder};
-       echo "New mount point is $dest." |tee -a ${log}/${1}_plot-${2}.log;
+       echo "New Mount Point is $dest." |tee -a ${log}/${1}_plot-${2}.log;
        break;
      fi
   done
 }
 
 # Main run loop
-for i in {1..2}
+for i in $(seq 1 $count);
 do
 
    dt=$(date '+%Y-%m-%d_%H_%M_%S');
@@ -49,11 +62,15 @@ do
 
    echo "Currently plotting number ${i} of 2 and started on ${dt}." |tee -a ${log}/${dt}_plot-${i}.log
    ./chia-plotter/build/chia_plot \
+   -r ${threads} \
+   -u ${buckets} \
+   -v ${buckets3} \
    -t ${tempdir} \
    -2 ${tempdir2} \
    -d ${dest} \
    -p ${pool} \
    -f ${farm} \
+   -G ${tmptoggle} \
    |tee -a ${log}/${dt}_plot-${i}.log
    echo "Time plot $i finished is $(date '+%H:%M:%S')." |tee -a ${log}/${dt_plot}-${i}.log
 done
