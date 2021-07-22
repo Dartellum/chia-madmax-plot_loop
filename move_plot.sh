5935421e9a9419dcf5425a121c155902d252f299
Dr. Walter Wesley Snyder V <wessnyder@comcast.net>
2021-07-21 20:35:55 -0400

    Updated Readme and move_plot.

--- move_plot.sh
+++ move_plot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+SOURCE_DIR=/media/tempdir
+DESTINATION_DIR=/media/chia/farm
 Discord=true
 url="https://discord.com/api/webhooks/yourhook"
-plot=$(find /media/tempdir -type f -name "*.plot")
+plot=$(find ${SOURCE_DIR} -type f -name "*.plot")
 
 discord_message () {
   json_payload=$(jq -nc --arg message "$message" '
@@ -22,22 +24,32 @@ if [[ ( -n $plot ) ]]; then
      exit
   fi
   ## Creating lock file ##
-  touch /media/tempdir/movingfiles.lock
+  touch ${SOURCE_DIR}/movingfiles.lock
 
-  ## Moving the plot files listed in $yourfilenames to destination - /media/chia a mergerfs Pool. ##
+  ## Moving the plot file(s) listed in $plots to destination - /media/chia a mergerfs Pool. ##
   if [ "${Discord}" = true ]; then
-      message="Move started at $(date '+%Y-%m-%d_%H:%M:%S') of file(s)."
+      message="Move on ${HOSTNAME} started at $(date '+%Y-%m-%d_%H:%M:%S') of file(s)."
       discord_message $message
   fi
 
-  mv /media/tempdir/*.plot /media/chia/farm
+  plots="$SOURCE_DIR"/*.plot
+
+  for eachplot in ${plots};
+  do
+    if [ "${Discord}" = true ]; then
+      message="Moving ${eachplot} on ${HOSTNAME} to farm. Started $(date '+%Y-%m-%d_%H:%M:%S')."
+      discord_message $message
+    fi
+    #mv /media/tempdir/${eachplot} ${DESTINATION_DIR}
+    rsync --remove-source-files --progress --partial --human-readable ${eachplot} ${DESTINATION_DIR}
+  done
 
   ##  Move complete, removing lock file ##
-  rm /media/tempdir/movingfiles.lock
+  rm ${SOURCE_DIR}/movingfiles.lock
 
   ## Lock file removed, copy complete ##
   if [ "${Discord}" = true ]; then
-    message="Move finished at $(date '+%Y-%m-%d_%H:%M:%S') of file(s)."
+    message="Move on ${HOSTNAME} of plot(s) finished at $(date '+%Y-%m-%d_%H:%M:%S')."
     discord_message ${message}
   fi
 fi
